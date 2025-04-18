09f71fab4297940c01a00af0189d5794c4757e66
Nipun Rajput <nipun.rajput6586s@gmail.com>
2025-04-18 10:15:40 +0530

    Logic Completed

--- blackjack/index.js
+++ blackjack/index.js
@@ -1,41 +1,55 @@
-let isAlive=false;
-let card=[];
-let firstCard=0;
-let secondCard=0;
-let cardStr=document.getElementById("card-id");
-let sumStr=document.getElementById("sum-id");
-let title=document.getElementById("display");
-cardStr.textContent= "Card: " + firstCard +" "+secondCard
-sumStr.textContent=firstCard+secondCard;
-title.textContent="Start a new round"
-function pickCard(){
-    firstCard=Random();
-    secondCard=Random();
-    card.push(firstCard);
-    card.push(secondCard);
-    isAlive=true;
-    if(sumStr.textContent<21){
-        title.textContent="Pick a New Card";
-    }else if(sumStr.textContent>21){
-        title.textContent="You Lose!!!";
-        isAlive=false;
-    }else if(sumStr.textContent==21){
-        title.textContent="BlackJack!!!"
-        isAlive=false;
+let isAlive = false;
+let card = [];
+let sum = 0;
+
+let cardStr = document.getElementById("card-id");
+let sumStr = document.getElementById("sum-id");
+let title = document.getElementById("display");
+
+function pickCard() {
+    card = [];
+    sum = 0;
+
+    let firstCard = Random();
+    let secondCard = Random();
+
+    card.push(firstCard, secondCard);
+    sum = firstCard + secondCard;
+
+    isAlive = true;
+    renderGame();
+}
+
+function newCard() {
+    if (isAlive && sum < 21) {
+        let newVal = Random();
+        card.push(newVal);
+        sum += newVal;
+        renderGame();
     }
 }
 
-function newCard(){
-    let newVal=Random();
-    // sumStr.textContent +=newVal;
+function renderGame() {
+    cardStr.textContent = "Cards: " + card.join(" ");
+    sumStr.textContent = "Sum: " + sum;
+
+    if (sum < 21) {
+        title.textContent = "Pick a New Card";
+    } else if (sum === 21) {
+        title.textContent = "BlackJack!!!";
+        isAlive = false;
+    } else {
+        title.textContent = "You Lose!!!";
+        isAlive = false;
+    }
 }
 
-function Random(){
-    let value=(Math.floor(Math.random()*12))+1;
-    if(value==1){
-        value=10;
-    }else if(value>10){
-        value=10;
+function Random() {
+    let value = Math.floor(Math.random() * 13) + 1;
+    if (value === 1) {
+        return 11;
+    } else if (value > 10) {
+        return 10;
     }
     return value;
 }
